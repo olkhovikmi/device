ac74c00692d90db535dc9ea658f93460952877ac
olkhovikmi <olkhovikmi3@gmail.com>
2020-06-29 17:06:03 +0300

    сделал popup

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,16 @@
+const write = document.querySelector('.contact-link'),
+	body = document.querySelector('.body'),
+	popup = document.querySelector('.popup'),
+	closePop = document.querySelector('.popup__close');
 
+write.addEventListener('click', function () {
+	if (popup.style.display == '') {
+		popup.style.display = 'block';
+	}
+});
+
+closePop.addEventListener('click', function () {
+	if (popup.style.display == 'block') {
+		popup.style.display = '';
+	}
+});
